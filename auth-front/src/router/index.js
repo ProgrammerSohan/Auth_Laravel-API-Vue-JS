9f464344820f23f76656833a50a82387f164c340
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-22 20:49:08 +0600

    registar from front

---
+++ auth-front/src/router/index.js
@@ -0,0 +1,22 @@
+
+import { createRouter, createWebHistory } from 'vue-router';
+import HelloWorld from '@/components/HelloWorld.vue';
+import RegisterPage from '@/components/RegisterPage.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+    {
+      path: '/register',
+      name: 'RegisterPage',
+      component: RegisterPage,
+    },
+  ],
+});
+
+export default router;
